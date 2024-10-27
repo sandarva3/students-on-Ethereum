a4bdf910d8c8aa0066af90d676f97d5a9221dfcc
Sandarva Paudel <sandarvapoudel182@gmail.com>
2024-10-27 08:38:36 +0545

    bug fixes

--- contracts/records.sol
+++ contracts/records.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity >= 0.5;
+pragma solidity >= 0.8.0;
 
 contract StudentRecords{
 
@@ -10,7 +10,7 @@ contract StudentRecords{
         uint rollNo;
         uint semester;
         mapping(uint => string) grade;
-        uint attendance;
+        mapping(uint => uint) attendance;
         address studentWallet;
         bool isRegistered;
     }
@@ -38,22 +38,27 @@ contract StudentRecords{
     }
 
 
-    function registerStudent(string memory name, uint rollno, uint sem, address key) public onlyOwner returns(bool){
+    function registerStudent(string memory name, uint rollno, uint sem, address key) public onlyOwner returns(uint, bool){
+        require(checkAddress(key), "The provided wallet key already exist");
+        require(checkRoll(rollno), "Student with given Roll no already exist");
         studentCount++;
-        require(!students[studentCount].isRegistered, "The Student already exist.");
 
         students[studentCount].id = studentCount;
         students[studentCount].name = name;
         students[studentCount].rollNo = rollno;
         students[studentCount].semester = sem;
-        students[studentCount].attendance = 0;
+        students[studentCount].attendance[sem] = 0;
         students[studentCount].studentWallet = key;
         students[studentCount].isRegistered = true;
+
+        return (studentCount, true);
     }
 
-    function getStudent(uint id) public view returns(string memory, uint, uint){
+    function getStudent(uint id) public view checkStudent(id) returns(string memory, uint, uint){
         return(students[id].name, students[id].rollNo, students[id].semester);
     }
+
+    //function getStudentFullInfo(uint id)
 
     function getWallet(uint id) public view returns(address){
         return(students[id].studentWallet);
@@ -79,9 +84,59 @@ contract StudentRecords{
         return true;
     }
 
+    function getGrade(uint id, uint sem) public view checkStudent(id) checkSem(id, sem) returns(string memory a){
+        return students[id].grade[sem];
+    }
+
+    //Semester wise grade
     function updateGrade(uint id, uint sem, string memory _grade) public onlyOwner checkStudent(id) returns(bool){
         require(bytes(students[id].grade[sem]).length != 0, "No grade. First add grade");
         students[id].grade[sem] = _grade;
         return true;
+    }
+
+    //Semester wise attendance
+    function updateAttendance(uint id, uint sem) public onlyOwner checkStudent(id) checkSem(id, sem) returns(bool){
+        students[id].attendance[sem] += 1;
+        return true;
+    }
+
+    function getAttendance(uint id, uint sem) public view checkStudent(id) checkSem(id, sem) returns(uint){
+        return students[id].attendance[sem];
+    }
+
+    function balanceOf(uint id) public checkStudent(id) view returns(uint){
+        address stuAddress = students[id].studentWallet;
+        return(address(stuAddress).balance);
+    }
+
+    function checkRoll(uint roll) public view onlyOwner returns(bool){
+        uint count = 0;
+        for(uint i = 1; i <= studentCount; i++){
+            if(students[i].rollNo == roll){
+                count++;
+            }
+        }
+        if(count != 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    function checkAddress(address stuAddress) public view onlyOwner returns(bool){
+        uint count = 0;
+        for(uint i = 1; i <= studentCount; i++){
+            if(students[i].studentWallet == stuAddress){
+                count++;
+            }
+        }
+        if(count != 0){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
